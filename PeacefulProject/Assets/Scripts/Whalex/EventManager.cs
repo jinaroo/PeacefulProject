f211fc0e6219ffb052fe974dacdc92365ad87fa6
Whalex <hsc19930818@gmail.com>
2019-02-14 11:53:14 -0500

    add some utility scripts

---
+++ PeacefulProject/Assets/Scripts/Whalex/EventManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using UnityEngine.Events;
+
+public class EventManager : Singleton<EventManager>
+{
+    private Dictionary<string, UnityEvent> eventDictionary = new Dictionary<string, UnityEvent>();
+/*    private static EventManager eventManager;
+
+    public static EventManager instance
+    {
+        get
+        {
+            if (!eventManager)
+            {
+                eventManager = FindObjectOfType(typeof(EventManager)) as EventManager;
+                if (!eventManager)
+                {
+                    Debug.LogError("can't find event manager!");
+                }
+                else
+                {
+                    eventManager.Init();
+                }
+            }
+
+            return eventManager;
+        }
+    }*/
+
+    public void StartListening(string eventName, UnityAction listener)
+    {
+        UnityEvent thisEvent;
+        if (Instance.eventDictionary.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent.AddListener(listener);
+        }
+        else
+        {
+            thisEvent = new UnityEvent();
+            thisEvent.AddListener(listener);
+            Instance.eventDictionary.Add(eventName, thisEvent);
+        }
+    }
+
+    public void StopListening(string eventName, UnityAction listener)
+    {
+        if (Instance == null) return;
+        
+        UnityEvent thisEvent;
+        if (Instance.eventDictionary.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent.RemoveListener(listener);
+        }
+    }
+
+    public void TriggerEvent(string eventName)
+    {
+        UnityEvent thisEvent;
+        if (Instance.eventDictionary.TryGetValue(eventName, out thisEvent))
+        {
+            thisEvent.Invoke();
+        }
+    }
+}
