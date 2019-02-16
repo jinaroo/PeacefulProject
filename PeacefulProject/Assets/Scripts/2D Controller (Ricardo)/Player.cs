05ff6674297a80e401dcd4d3d54faec586aac0e1
r-escobar <rescobar713@gmail.com>
2019-02-16 15:11:33 -0500

    adjusted visibility of several movement variables in Player.cs

--- PeacefulProject/Assets/Scripts/2D Controller (Ricardo)/Player.cs
+++ PeacefulProject/Assets/Scripts/2D Controller (Ricardo)/Player.cs
@@ -7,9 +7,9 @@ public class Player : MonoBehaviour {
 	public float maxJumpHeight = 4;
 	public float minJumpHeight = 1;
 	public float timeToJumpApex = .4f;
-	float accelerationTimeAirborne = .2f;
-	float accelerationTimeGrounded = .1f;
-	float moveSpeed = 6;
+	public float accelerationTimeAirborne = .2f;
+	public float accelerationTimeGrounded = .1f;
+	public float moveSpeed = 6;
 
 	public Vector2 wallJumpClimb;
 	public Vector2 wallJumpOff;
