6e7d1f34d3928b876743a390ab1032fbd76921ad
jinaroo <sohnjin@gmail.com>
2019-03-10 19:13:49 -0400

    added test npc interaction to jin test scene

---
+++ PeacefulProject/Assets/Scripts/Jin/PopUp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUp : MonoBehaviour
+{
+    public GameObject popUp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        popUp.SetActive(false);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        popUp.SetActive(true);
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        popUp.SetActive(false);
+
+    }
+}
