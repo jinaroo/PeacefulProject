402ca8aa443b62f32b87206acae8705a3985d75d
maryageorgescu <mg3200@nyu.edu>
2019-03-26 09:02:56 -0400

    Added an Audio Manager Script.

---
+++ PeacefulProject/Assets/Scripts/Mary/AudioClips.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Intent: A script to allow us to access/change out sounds as needed
+//Usage: Just let it hang out
+
+public class AudioClips : MonoBehaviour
+{
+
+
+//Item sound effects
+    public AudioClip pickUpItem;
+    public AudioClip pushItem;
+    public AudioClip[] breakItem;
+    
+//player sound effects
+    public AudioClip[] PlayerWalk; //sounds different depending on surface.
+    public AudioClip[] playerJump; //sounds different depending on surface.
+    public AudioClip[] playerClimb; //might wanna have the climb sound diferent/ more difficult if it takes longer
+    public AudioClip[] PlayerSlide; //sounds different depending on surface.
+    
+    //dog sound effects
+    public AudioClip[] dogHappy;
+    public AudioClip[] dogSad;
+    public AudioClip[] dogQuest;
+    public AudioClip[] dogReward;
+    
+    //bird sound effects
+    public AudioClip[] birdHappy;
+    public AudioClip[] birdSad;
+    public AudioClip[] birdQuest;
+    public AudioClip[] birdReward;
+    
+    //hamster sound effects
+    public AudioClip[] hamsterHappy;
+    public AudioClip[] hamsterSad;
+    public AudioClip[] hamsterQuest;
+    public AudioClip[] hamsterReward;
+    
+    //enviroment sound effects
+    public AudioClip treeShuffle;
+    public AudioClip[] rockShuffle;
+    public AudioClip rockHitSword;
+    public AudioClip daggerHitSword;
+    
+    //narrative sound effect for thought bubble stories
+    public AudioClip dogStoryBubble;
+    public AudioClip birdStoryBubble;
+    public AudioClip hamsterStoryBubble;
+}
+
+
+
+
---
+++ PeacefulProject/Assets/Scripts/Mary/AudioManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Intent: To have a script that manages the audio for the game
+//Usage: Place on an empty Gameobject in the scene named Audio Manager
+
+public class AudioManager : MonoBehaviour
+{
+	public AudioClips Clips;
+	public static AudioManager Instance;
+
+	private void Awake()
+	{
+		if (Instance == null)
+		{
+			Instance = this;
+			DontDestroyOnLoad(gameObject);
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	public void Start()
+	{
+		Clips = Resources.Load<GameObject>("Clips").GetComponent<AudioClips>();
+	}
+
+	public GameObject PlaySoundEffect(AudioClip audioClip, float volume = 1.0f, bool looping = false)
+	{
+		var newGameObject = new GameObject("Audio Effect");
+		newGameObject.transform.position = Camera.main.transform.position;
+		
+		var newAudioSource = newGameObject.AddComponent<AudioSource>();
+		newAudioSource.spatialize = false;
+
+		newAudioSource.clip = audioClip;
+		newAudioSource.volume = volume;
+		newAudioSource.loop = looping;
+
+		newAudioSource.Play();
+
+		if (!looping)
+		{
+			Destroy(newGameObject, audioClip.length * 3.0f);
+		}
+
+		return newGameObject;
+	}
+}
+
